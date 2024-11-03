9644ef2fa40f1dafd9a92c2c9a1ea1554f221a22
kamil Adamowski <ka.adamowski@gmail.com>
2024-11-03 21:08:48 +0100

    Add logo, new title

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ButtonMailto from '../../common/ButtonMailto';
 import { MdEmail } from "react-icons/md";
+import MyLogo from '../../../public/logo.svg';
+
 
 export const Navbar = () => (
   <header className='top-0 z-40 flex-none w-full h-16'>
     <div className='px-4 py-4'>
       <div className='relative flex items-center'>
         <div className='flex-none mr-8'>
-          <h1 className='text-4xl font-bold text-dark'>KA</h1>
+        <img src={MyLogo} alt='logo' className='w-16' />
         </div>
         <nav className='ml-auto' aria-label="Main navigation">
           <ul className='flex space-x-8'>
